ceddffa137a1e71656f63bf7b37aee7902a36e64
Evelyn House <eve@ehouse.io>
2024-02-23 18:52:55 -0500

    [v1.3.2] Fix types to be correctly exported from project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-//import { evaluate } from "./evaluator"
-//import { parse } from "./parser"
-//import type { EvalExpression } from "./types"
-
-export { exec, evaluate } from './evaluator'
-export { parse } from './parser'
+export * from './types'
+export * from './evaluator'
+export * from './parser'
