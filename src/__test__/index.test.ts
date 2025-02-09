10072bda8d415300c20c827a8d9cb23d72b6e1f0
Evelyn House <eve@ehouse.io>
2025-02-09 13:35:33 -0500

    Migrating from jest to vitest

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,22 +1,23 @@
 import { cmdEvaluate, exec } from "../evaluator";
 import { parse } from "../parser";
+import { describe, test, expect } from "vitest";
 
 describe("test single parser", () => {
-  it("single number", () => {
+  test("single number", () => {
     expect(parse("1")).toEqual({ n: 1, tag: "number" });
   });
 
-  it("single die roll", () => {
+  test("single die roll", () => {
     expect(parse("1d20")).toEqual({ n: 1, sides: 20, tag: "roll" });
   });
 
-  it("multiple die roll", () => {
+  test("multiple die roll", () => {
     expect(parse("2d20")).toEqual({ n: 2, sides: 20, tag: "roll" });
   });
 });
 
 describe("Test probability statistics", () => {
-  it("Rolling 1d20", () => {
+  test("Rolling 1d20", () => {
     const expression = exec("1d20");
     expect(expression.stats).toEqual({
       avg: 10.5,
@@ -25,7 +26,7 @@ describe("Test probability statistics", () => {
     });
   });
 
-  it("Rolling (10d20 + 10) * 2", () => {
+  test("Rolling (10d20 + 10) * 2", () => {
     const expression = exec("(10d20 + 10) * 2");
     expect(expression.stats).toEqual({
       avg: 230,
@@ -36,7 +37,7 @@ describe("Test probability statistics", () => {
 });
 
 describe("Test CMD", () => {
-  it("evaluates complex athematic string", () => {
+  test("evaluates complex athematic string", () => {
     expect(cmdEvaluate("(2 + 2) * 2")).toEqual(
       "2 + 2 * 2 > 8 {min: 8, max: 8, avg: 8 }"
     );
